59274d3a974270a8e9833a491ab690a372b10a64
Albert <astarreveld@xebia.com>
2020-01-29 16:31:53 +0100

    Added tests for DriveCustomerToTrainStationMapper

---
+++ Tests/Dispatching.Broker.Tests/UnitTests/Commands/Mappers/DriveCustomerToTrainStationMapperTests/MapToCustomerIdTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using Dispatching.Broker.Commands;
+using Dispatching.Broker.Commands.Mappers;
+using Dispatching.Customers;
+using Dispatching.Framework;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dispatching.Broker.Tests.UnitTests.Commands.Mappers.DriveCustomerToTrainStationMapperTests
+{
+    [TestClass]
+    [TestCategory("UnitTests")]
+    public class MapToCustomerIdTest
+    {
+        private Fixture _fixture = new Fixture();
+
+        [TestMethod]
+        public void WhenCustomerId_ShouldMapToId()
+        {
+            // Arrange
+            var input = _fixture.Create<DriveCustomerToTrainStation>();
+
+            // Act
+            var sut = new DriveCustomerToTrainStationMapper();
+            var actual = sut.MapToCustomerId(input);
+
+            // Assert
+            var expected = new Id<Customer>(input.CustomerId);
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ Tests/Dispatching.Broker.Tests/UnitTests/Commands/Mappers/DriveCustomerToTrainStationMapperTests/MapToCustomerLocationTest.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture;
+using Dispatching.Broker.Commands;
+using Dispatching.Broker.Commands.Mappers;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dispatching.Broker.Tests.UnitTests.Commands.Mappers.DriveCustomerToTrainStationMapperTests
+{
+    [TestClass]
+    [TestCategory("UnitTests")]
+    public class MapToCustomerLocationTest
+    {
+        private Fixture _fixture = new Fixture();
+
+        [TestMethod]
+        public void WhenCurrentLongitude_ShouldMapToLongitude()
+        {
+            // Arrange
+            var input = _fixture.Create<DriveCustomerToTrainStation>();
+
+            // Act
+            var sut = new DriveCustomerToTrainStationMapper();
+            var actual = sut.MapToCustomerLocation(input);
+
+            // Assert
+            actual.Longitude.Should().Be(input.CurrentLongitude);
+        }
+
+
+        [TestMethod]
+        public void WhenCurrentLatitude_ShouldMapToLatitude()
+        {
+            // Arrange
+            var input = _fixture.Create<DriveCustomerToTrainStation>();
+
+            // Act
+            var sut = new DriveCustomerToTrainStationMapper();
+            var actual = sut.MapToCustomerLocation(input);
+
+            // Assert
+            actual.Latitude.Should().Be(input.CurrentLatitude);
+        }
+    }
+}
