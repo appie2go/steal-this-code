f05890a999f3a6298f973a4cac19a0352607fb25
Albert <astarreveld@xebia.com>
2020-01-29 14:49:30 +0100

    Added tests for the GetDistanceResponseMapper

---
+++ Tests/Dispatching.Aaa.Tests/UnitTests/Mappers/GetDistanceResponseMapperTests/MapTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using Dispatching.Aaa.Mappers;
+using Dispatching.Rides;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dispatching.Aaa.Tests.UnitTests.Mappers.GetDistanceResponseMapperTests
+{
+    [TestClass]
+    public class MapTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        private TheirSchema _theirResponse;
+
+        private GetDistanceResponseMapper _mapper;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _theirResponse = _fixture.Create<TheirSchema>();
+
+            _mapper = new GetDistanceResponseMapper();
+        }
+
+        [TestMethod]
+        public void WhenAaaPopulatesEtaField_ShouldDeserializeTheirJsonToKilometers()
+        {
+            // Arrange
+            var httpResponseMessage = _theirResponse.ToHttpResponseMessage();
+
+            // Act
+            var actual = _mapper.Map(httpResponseMessage);
+
+            // Assert
+            var expected = Kilometer.FromDecimal(_theirResponse.Kilometers);
+            actual.Should().Be(expected);
+        }
+
+        public class TheirSchema
+        {
+            public int Kilometers { get; set; }
+        }
+    }
+}
