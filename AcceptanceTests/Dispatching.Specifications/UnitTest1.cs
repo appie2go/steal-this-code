835fd3e0b30d9ede008c87591879a8bb2047f25f
Albert <astarreveld@xebia.com>
2020-02-02 12:10:04 +0100

    Added acceptance testing assembly

---
+++ AcceptanceTests/Dispatching.Specifications/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dispatching.Specifications
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
