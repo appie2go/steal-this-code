1a8db27a3137b5e5eb2d656207b6028c3b92aeba
Albert <astarreveld@xebia.com>
2020-02-02 13:05:34 +0100

    Added rough outline for the test context

---
+++ AcceptanceTests/Dispatching.Specifications/TestContext/InMemoryDbContexFactory.cs
@@ -0,0 +1,48 @@
+﻿using Dispatching.Persistence;
+using Dispatching.ReadModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Dispatching.Specifications.TestContext
+{
+    internal class InMemoryDbContexFactory
+    {
+        public const string ScenarioKey = "databasename";
+
+        private readonly string _databaseName;
+
+        public InMemoryDbContexFactory(ScenarioContext scenarioContext)
+        {            
+            if (!scenarioContext.ContainsKey(ScenarioKey)) 
+            {
+                throw new InvalidOperationException($"Cannot construct a db context for the acceptance tests if the scenario context does not" +
+                    $"contain a key named '{ScenarioKey}' that contains a string value. Make it's set before the scenario starts.");
+            }
+
+            _databaseName = (string)scenarioContext[ScenarioKey];
+        }
+
+        public DispatchingDbContext CreateWriteContext()
+        {
+            var databaseName = $"{_databaseName}{nameof(DispatchingDbContext)}";
+            var options = CreateOptions<DispatchingDbContext>(databaseName);
+            return new DispatchingDbContext(options);
+        }
+
+        public DispatchingReadDbContext CreateReadContext()
+        {
+            var databaseName = $"{_databaseName}{nameof(DispatchingReadDbContext)}";
+            var options = CreateOptions<DispatchingReadDbContext>(databaseName);
+            return new DispatchingReadDbContext(options);
+        }
+
+        private static DbContextOptions<T> CreateOptions<T>(string databaseName) where T : DbContext
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<T>();
+            return optionsBuilder
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+        }
+    }
+}
---
+++ AcceptanceTests/Dispatching.Specifications/TestContext/TestCase.cs
@@ -0,0 +1,41 @@
+﻿using Dispatching.Persistence;
+using Dispatching.ReadModel;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using TechTalk.SpecFlow;
+
+namespace Dispatching.Specifications.TestContext
+{
+    internal abstract class TestCase
+    {
+        public void Apply(ScenarioContext context)
+        {
+            var dbContextFactory = new InMemoryDbContexFactory(context);
+
+            using (var dbContext = dbContextFactory.CreateWriteContext())
+            {
+                Apply(dbContext);
+            }
+
+            using (var readContext = dbContextFactory.CreateReadContext())
+            {
+                Apply(readContext);
+            }
+        }
+
+        protected abstract void Apply(DispatchingDbContext dispatchingDbContext);
+        protected abstract void Apply(DispatchingReadDbContext dispatchingDbContext);
+    }
+
+    internal abstract class TestCase<T> where T : class
+    {
+        public void Apply(IServiceCollection serviceCollection)
+        {
+            var substitute = Substitute.For<T>();
+            Apply(substitute);
+            serviceCollection.AddTransient((s) => substitute);
+        }
+
+        protected abstract void Apply(T substitute);
+    }
+}
